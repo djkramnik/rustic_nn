6b00303457640c27bb0fa4cb41a0ccc4eeb5f2f1
David Gurr <davidk.gurr@gmail.com>
2025-12-04 14:07:19 -0500

    the layer layer

---
+++ src/layer/dense.py
@@ -0,0 +1,41 @@
+from layer.layer import Layer
+from operation.activation import Sigmoid
+from operation.operation import Operation
+from operation.weightmultiply import WeightMultiply
+from operation.bias_add import BiasAdd
+from util.customtypes import *
+from typing import Type
+import numpy as np
+
+class Dense(Layer):
+  '''
+  A fully connected layer that inherits from Layer
+  '''
+  def __init__(self, neurons: int, activation: Type[Operation] = Sigmoid):
+    super().__init__(neurons)
+    self.activation = activation()
+
+  def _setup_layer(self, input_: ndarray):
+    # thank you benefactor
+    if (self.seed):
+      np.random.seed(self.seed)
+
+    # this will get wiped out by calling _params
+    self.params = []
+
+    # matmul weight param is (#features, hidden_size)
+    # should we not normalize the weights?
+    self.params.append(
+      np.random.randn(input_.shape[1], self.neurons)
+    )
+    # bias param
+    # bias weights are (1, hidden_size)
+    self.params.append(np.random.randn(1, self.neurons))
+
+    self.operations = [
+      WeightMultiply(self.params[0]),
+      BiasAdd(self.params[1]),
+      self.activation
+    ]
+
+
---
+++ src/layer/layer.py
@@ -0,0 +1,53 @@
+from util.customtypes import *
+from util.assert_shape import assert_shape
+from operation.operation import Operation
+
+# issubclass _should_ work and is the better way?
+def has_ndarray(op: Operation, attr: str) -> bool:
+  return str(type(getattr(op, attr, None))) == "<class 'numpy.ndarray'>"
+
+class Layer(object):
+  def __init__(self, neurons:int):
+    self.neurons = neurons
+    self.first = True
+    self.params: List[ndarray] = []
+    self.param_grads: List[ndarray] = []
+    self.operations: List[Operation] = []
+  # self requires input.. which is why setup layer is called from forward
+  # I assume it needs it to initialize random weights of the corresponding shape
+
+  def _setup_layer(self, input_: ndarray):
+    raise NotImplementedError()
+
+  def forward(self, input_: ndarray) -> ndarray:
+    if (self.first):
+      self._setup_layer(input_)
+      self.first = False
+
+    # unsure whether this gets used anywhere but we cache
+    # the last input passed to forward
+    self.input_ = input_
+    for operation in self.operations:
+      input_ = operation.forward(input_)
+    self.output = input_
+    return self.output
+
+  def backward(self, output_grad: ndarray) -> ndarray:
+    assert_shape(self.output, output_grad)
+    for operation in reversed(self.operations):
+      output_grad = operation.backward(output_grad)
+
+    self._param_grads()
+    return output_grad
+
+  def _param_grads(self) -> ndarray:
+    self.param_grads = []
+    for op in self.operations:
+      if (has_ndarray(op, 'param_grad')):
+        self.param_grads.append(op.param_grad)
+
+  def _params(self) -> ndarray:
+    self.params = []
+    for op in self.operations:
+      if (has_ndarray(op, 'param')):
+        self.params.append(op.param)
