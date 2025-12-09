7ebd129deacb3407a08b9874c7697704a78ceb3b
David Gurr <davidk.gurr@gmail.com>
2025-12-09 12:53:05 -0500

    pokeman trainer

---
+++ src/trainer/trainer.py
@@ -0,0 +1,91 @@
+import numpy as np
+from copy import deepcopy
+from typing import Iterator
+from neural_network.nn import NeuralNetwork
+from optimizer.optimizer import Optimizer
+from util.customtypes import *
+
+# shuffle X; same shuffle for y
+def permute_data(X: ndarray, y: ndarray) -> tuple[ndarray, ndarray]:
+  perm = np.random.permutation(X.shape[0])
+  return X[perm], y[perm]
+
+class Trainer(object):
+  def __init__(
+    self,
+    net: NeuralNetwork,
+    optim: Optimizer
+  ):
+    '''
+    Requires a neural network and an optimizer
+    assign the neural network as an attribute of the optimizer
+    '''
+    self.net = net
+    self.optim = optim
+    setattr(self.optim, 'net', self.net)
+
+  def generate_batches(self, X: ndarray, y: ndarray, size: int=32) -> Iterator[tuple[ndarray, ndarray]]:
+    '''
+    Given the complete training dataset, split into batches of size size
+    '''
+    N = X.shape[0]
+    assert N == y.shape[0]
+    for i in range(0, N, size):
+      X_batch = X[i:i+size]
+      y_batch = y[i:i+size]
+      yield X_batch, y_batch
+
+  # do everything
+  def fit(self,
+    X_train: ndarray,
+    y_train: ndarray,
+    X_test: ndarray,
+    y_test: ndarray,
+    epochs: int = 100,
+    eval_every: int = 10,
+    batch_size: int = 32,
+    seed: int = 1,
+    restart: bool = True
+    ) -> None:
+    '''
+    Fits the neural network on the training data for a certain number
+    of epochs. Run validation every eval_every iterations
+    '''
+    # why considering what else we have
+    np.random.seed(seed)
+    # this will cause each layer to invoke setup_layer.
+    # in the case of Dense this reinstantiates the ParamOperations with fresh params
+    if restart:
+      for layer in self.net.layers:
+        layer.first = True
+      self.best_loss = 1e9
+
+    best_model = deepcopy(self.net)
+
+    for e in range(epochs):
+      # before chunking, shuffle
+      X_train, y_train = permute_data(X_train, y_train)
+      batch_generator = self.generate_batches(
+        X_train, y_train, batch_size
+      )
+      for(X_batch, y_batch) in batch_generator:
+        self.net.train_batch(X_batch, y_batch)
+        self.optim.step()
+
+      # separate from training.  eval for log and early stop
+      if (e + 1) % eval_every == 0:
+        test_preds = self.net.forward(X_test)
+        loss = self.net.loss.forward(test_preds, y_test)
+        if loss < self.best_loss:
+          print(f"Validation loss after {e+1} epochs is {loss:.3f}")
+          self.best_loss = loss
+          best_model = deepcopy(self.net)
+        else:
+          break
+
+    self.net = best_model
+    setattr(self.optim, 'net', self.net)
+
+
+
+
