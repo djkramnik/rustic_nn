357e4a36aa1690c7b47c9fbccd4206dc5cb82be7
David Gurr <davidk.gurr@gmail.com>
2026-01-09 13:38:48 -0500

    replicate book style binary cross entropy

--- src/raw/loss.py
+++ src/raw/loss.py
@@ -8,8 +8,14 @@ def softmax_safe(x: np.ndarray) -> np.ndarray:
   max = np.max(x, axis=axis, keepdims=True)
   return np.exp(x - max) / np.sum(np.exp(x - max), axis=axis, keepdims=True)
 
+# It assumes target is one-hot (N, C)
+def binary_cross_entropy(softmax_preds: np.ndarray, target: np.ndarray):
+  # prevent log(0)
+  p = np.clip(softmax_preds, 1e-9, (1 - 1e-9))
+  q = target
 
-# impl this next please
-def binary_cross_entropy(preds: np.ndarray, target: np.ndarray):
-  # apply softmax in here
-  pass
+  loss = (-q* np.log(p)) - ((1 - q) * np.log(1 - p))
+  return np.sum(loss) / softmax_preds.shape[0]
+
+def softmax_ce(preds: np.ndarray, target: np.ndarray) -> np.ndarray:
+  return binary_cross_entropy(softmax_safe(preds), target)
