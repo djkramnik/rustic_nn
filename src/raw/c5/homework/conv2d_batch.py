47b4df30021d0c4761368cc29998c34f77092130
David Gurr <davidk.gurr@gmail.com>
2026-03-11 12:06:05 -0400

    babys first conv2d batch

--- src/raw/c5/homework/conv2d_batch.py
+++ src/raw/c5/homework/conv2d_batch.py
@@ -1,8 +1,8 @@
 import numpy as np
 from numpy import ndarray
 
-from raw.c5.helper import assert_dim
-from raw.c5.homework.conv2d import conv_2d_fwd, pad_2d
+from raw.c5.helper import assert_dim, assert_same_shape
+from raw.c5.homework.conv2d import conv_2d_fwd, conv_2d_inp_grad, conv_2d_param_grad, pad_2d
 
 # this time we have batches of square tensors (batch_size, img_w, img_h)
 def pad_2d_batch(arr: ndarray, size = 1):
@@ -16,5 +16,28 @@ def conv_2d_batch_fwd(arr: ndarray, param: ndarray):
   assert_dim(param, 2)
   list = [conv_2d_fwd(obs, param) for obs in arr]
   return np.stack(list)
+
+def conv_2d_batch_inpgrad(arr: ndarray, param: ndarray, outgrad: ndarray = None):
+  if outgrad is None:
+    outgrad = np.ones_like(arr)
+  else:
+    assert_same_shape(arr, outgrad)
+
+  grads = [conv_2d_inp_grad(obs, param, outgrad[i]) for (i, obs) in enumerate(arr)]
+  return np.stack(grads)
+
+def conv_2d_batch_paramgrad(
+    arr: ndarray,
+    param: ndarray,
+    outgrad: ndarray=None):
+  if outgrad is None:
+    outgrad = np.ones_like(arr)
+  else:
+    assert_same_shape(arr, outgrad)
+
+  param_grad = np.zeros_like(param)
+  for (i, obs) in enumerate(arr):
+    param_grad += conv_2d_param_grad(obs, param, outgrad[i])
+  return param_grad
 
 
--- src/raw/c5/tests/test_conv2dbatch.py
+++ src/raw/c5/tests/test_conv2dbatch.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-from raw.c5.conv_2d import _compute_output_2d, _pad_2d
-from raw.c5.homework.conv2d_batch import conv_2d_batch_fwd, pad_2d_batch
+from raw.c5.conv_2d import _compute_grads_2d, _compute_output_2d, _pad_2d, _param_grad_2d
+from raw.c5.homework.conv2d_batch import conv_2d_batch_fwd, conv_2d_batch_inpgrad, conv_2d_batch_paramgrad, pad_2d_batch
 
 np.random.seed(190220)
 imgs_2d_batch = np.random.randn(3, 28, 28)
@@ -16,3 +16,24 @@ def test_conv2d_batch_fwd():
   book_fwd = _compute_output_2d(imgs_2d_batch, param_2d)
   brain_fwd = conv_2d_batch_fwd(imgs_2d_batch, param_2d)
   assert np.allclose(book_fwd, brain_fwd)
+
+def test_conv2d_batch_inpgrad():
+  outgrad = np.ones_like(imgs_2d_batch)
+  book_inp_grad = _compute_grads_2d(imgs_2d_batch, outgrad, param_2d)
+  brain_inp_grad = conv_2d_batch_inpgrad(imgs_2d_batch, param_2d, outgrad)
+  assert np.allclose(book_inp_grad, brain_inp_grad)
+  outgrad2 = imgs_2d_batch + 1
+  book_inp_grad2 = _compute_grads_2d(imgs_2d_batch, outgrad2, param_2d)
+  brain_inp_grad2 = conv_2d_batch_inpgrad(imgs_2d_batch, param_2d, outgrad2)
+  assert np.allclose(book_inp_grad2, brain_inp_grad2)
+
+def test_conv2d_batch_paramgrad():
+  outgrad = np.ones_like(imgs_2d_batch)
+  book_param_grad = _param_grad_2d(imgs_2d_batch, outgrad, param_2d)
+  brain_param_grad = conv_2d_batch_paramgrad(imgs_2d_batch, param_2d, outgrad)
+  assert np.allclose(book_param_grad, brain_param_grad)
+
+  outgrad2 = imgs_2d_batch + 1
+  book_param_grad2 = _param_grad_2d(imgs_2d_batch, outgrad2, param_2d)
+  brain_param_grad2 = conv_2d_batch_paramgrad(imgs_2d_batch, param_2d, outgrad2)
+  assert np.allclose(book_param_grad2, brain_param_grad2)
