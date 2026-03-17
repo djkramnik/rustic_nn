3e5ff29fcf4aa38280dd3ed5f84fd5cd409869d5
David Gurr <davidk.gurr@gmail.com>
2026-03-16 23:57:03 -0400

    smol victory

--- src/raw/c5/homework/conv2d_full.py
+++ src/raw/c5/homework/conv2d_full.py
@@ -1,2 +1,39 @@
 import numpy as np
+from numpy import ndarray
 
+from raw.c5.conv_2d_channels import _pad_conv_input
+from raw.c5.helper import assert_dim
+from raw.c5.homework.conv2d import conv_2d_fwd
+from raw.c5.homework.conv2d_batch import pad_2d_batch
+
+# this is still only padding the last two dims?
+def pad_2d_full(arr: ndarray, size = 1):
+  assert_dim(arr, 4)
+  padded_chans = [pad_2d_batch(chan, size) for chan in arr]
+  return np.stack(padded_chans)
+
+# cifar_imgs = np.random.randn(10, 3, 32, 32)
+
+# padded_cifar = pad_2d_full(cifar_imgs, 1)
+# padded_cifar_book = _pad_conv_input(cifar_imgs, 1)
+# assert np.allclose(padded_cifar.shape, [10, 3, 34, 34])
+# assert np.allclose(padded_cifar.shape, padded_cifar_book.shape)
+
+
+def conv_2d_full(arr: ndarray, param: ndarray):
+  assert_dim(arr, 4)
+  assert_dim(param, 4)
+  assert arr.shape[1] == param.shape[0]
+
+  output = np.zeros((arr.shape[0], param.shape[1], arr.shape[2], arr.shape[3]))
+  for i in range(arr.shape[0]):
+    for j in range(param.shape[0]):
+      for k in range(param.shape[1]):
+        output[i][k] += conv_2d_fwd(arr[i][j], param[j][k])
+
+  return output
+
+## 2, 1, 4, 4
+## 1, 2, 3, 3
+
+# print(cifar_imgs[0][0].shape)
--- src/raw/c5/tests/test_conv2d_full.py
+++ src/raw/c5/tests/test_conv2d_full.py
@@ -1,1 +1,34 @@
 import numpy as np
+
+from raw.c5.conv_2d_channels import _output
+from raw.c5.homework.conv2d_full import conv_2d_full
+
+np.random.seed(190220)
+cifar_imgs = np.random.randn(10, 3, 32, 32)
+cifar_param = np.random.randn(3, 16, 5, 5)
+
+# def test_fwd():
+#   output_brain = conv_2d_full(cifar_imgs, cifar_param)
+#   book_brain = _output(cifar_imgs, cifar_param)
+#   assert np.allclose(output_brain, book_brain)
+
+def test_fwd_lite():
+  batcha = np.arange(1, 17).reshape((4,4))
+  batchb = np.arange(2, 18).reshape((4,4))
+  c = np.expand_dims(np.stack((batcha, batchb)), axis=1)
+  assert np.allclose(c.shape, (2, 1, 4, 4))
+
+  parama = np.arange(9).reshape((3,3))
+  paramb = np.arange(1,10).reshape((3,3))
+  params = np.expand_dims(np.stack((parama, paramb)), axis=0)
+  assert np.allclose(params.shape, (1, 2, 3, 3))
+
+  output = _output(c, params)
+  output_brain = conv_2d_full(c, params)
+  assert np.allclose(output, output_brain)
+  assert np.allclose(output_brain.shape, (2, 2, 4, 4))
+  assert np.allclose(output_brain[0][0][0][0], 97)
+  assert np.allclose(output_brain[0][1][0][0], 111)
+  assert np.allclose(output_brain[1][0][0][0], 121)
+  assert np.allclose(output_brain[1][1][0][0], 139)
+
