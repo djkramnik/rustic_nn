eb18d543cb28a51f8787846ec58c5d66f5117e3d
David Gurr <davidk.gurr@gmail.com>
2026-03-30 11:46:54 -0400

    autodiff again

---
+++ src/raw/c5/autodiff.py
@@ -0,0 +1,80 @@
+from typing import List, Union
+
+import numpy as np
+
+type Numerable = Union[float, int, "NumWithGrad"]
+
+def to_number_grad(num: Numerable) -> "NumWithGrad":
+  if isinstance(num, "NumWithGrad"):
+    return num
+  return NumWithGrad(num)
+
+
+class NumWithGrad(object):
+  def __init__(self,
+    num: Numerable,
+    depends_on: List[Numerable] = None,
+    creation_op: str = ''):
+    self.num = num
+    self.grad = None
+    self.depends_on = depends_on or []
+    self.creation_op = creation_op
+
+  def __add__(self, other: Numerable) -> "NumWithGrad":
+    return NumWithGrad(
+      self.num + to_number_grad(other).num,
+      depends_on = [self, to_number_grad(other)],
+      creation_op = 'add'
+      )
+
+  def __mul__(self, other: Numerable) -> "NumWithGrad":
+    return NumWithGrad(
+      self.num * to_number_grad(other).num,
+      depends_on=[self, to_number_grad(other)],
+      creation_op = 'mul'
+    )
+
+  def backward(self, backward_grad: "NumWithGrad" = None) -> None:
+    if backward_grad is None:
+      # first time apparently
+      self.grad = 1
+    else:
+      # these lines allow gradients to accumulate
+      # if the gradient doesn't exist yet.. which is not supposed to be possible..
+      # set as equal to backward_grad
+      if self.grad is None:
+        self.grad = backward_grad
+      else:
+        self.grad += backward_grad
+
+    if self.creation_op == 'add':
+      # wont' this recurse endlessly?
+      # simply send backward self.grad, since increasing either of these
+      # elements will increase the output by that same amount
+      self.depends_on[0].backward(self.grad)
+      self.depends_on[1].backward(self.grad)
+
+    if self.creation_op == 'mul':
+      # also endless recursion afaict
+
+      # calculate the derivative with respect to the first element
+      doutdfirst = self.depends_on[1] * self.grad
+      # send backward the derivative with respect to that element
+      self.depends_on[0].backward(doutdfirst.num)
+
+      # calculate the derivative with respect to the second element
+      doutdsecond = self.depends_on[0] * self.grad
+      # send backward the derivative with respect to that element
+      self.depends_on[1].backward(doutdsecond.num)
+
+
+
+
+
+
+
+
+
+
+
+
--- src/raw/c5/homework/scratch.py
+++ src/raw/c5/homework/scratch.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from raw.c5.conv_2d_channels import _param_grad
 from raw.c5.helper import assert_dim
 from raw.c5.homework.conv2d import conv_2d_param_grad
 from raw.c5.homework.conv2d_full import conv_2d_full
@@ -17,21 +18,45 @@ def conv_squares(arr, param):
   pw = param.shape[0]
   pad_sz = pw // 2
   padded_inp = pad(arr, pad_sz)
-  output = np.zeros_like(arr)
+  output = np.zeros(arr.shape)
   for i in range(arr.shape[0]):
     for j in range(arr.shape[1]):
       window = padded_inp[i:i+pw,j:j+pw]
       output[i][j] = np.sum(window * param)
   return output
 
+def param_grad_squares(inp, param, outgrad = None):
+  if (outgrad is None):
+    outgrad = np.ones(inp.shape)
+  else:
+    assert np.allclose(outgrad.shape, inp.shape)
+  pw = param.shape[0]
+  pad_sz = pw // 2
+  padded_inp = pad(inp, pad_sz)
+  param_grad = np.zeros(param.shape)
+
+  for i in range(inp.shape[0]):
+    for j in range(inp.shape[1]):
+      window = padded_inp[i:i+pw,j:j+pw]
+      param_grad += (window * outgrad[i][j])
+  return param_grad
+
 # print(conv_squares(a, b))
 
-param_grad = conv_2d_param_grad(a, b)
+ra = np.random.randn(2,2)
+rb = np.random.randn(3,3)
+
+param_grad = conv_2d_param_grad(ra, rb)
+param_gradish = param_grad_squares(ra, rb)
+assert np.allclose(param_grad, param_gradish)
 # print(param_grad)
 
-outgrad = np.array([4,1,2,3]).reshape((2,2))
-param_grad_2 = conv_2d_param_grad(a, b, outgrad)
-# print(param_grad_2)
+outgrad = np.random.randn(2,2)
+
+# print(outgrad)
+param_grad_2 = conv_2d_param_grad(ra, rb, outgrad)
+param_gradish_2 = param_grad_squares(ra, rb, outgrad)
+assert np.allclose(param_grad_2, param_gradish_2)
 
 
 full_inp = np.array([1, 2, 3, 4, 2, 4, 1, 3]).reshape((1,2,2,2))
@@ -84,8 +109,36 @@ def da_full_fwd(inp, param):
   return out
 
 full_out = da_full_fwd(full_inp, full_param)
-print(full_out.shape)
+# print(full_out.shape)
 
 full_out_prior = conv_2d_full(full_inp, full_param)
 
 assert np.allclose(full_out, full_out_prior)
+
+def da_full_paramgrad(inp, param, outgrad = None):
+  assert_dim(inp, 4)
+  assert_dim(param, 4)
+  assert inp.shape[1] == param.shape[0]
+  if outgrad is None:
+    outgrad = np.ones((inp.shape[0], param.shape[1], inp.shape[2], inp.shape[3]))
+  else:
+    assert np.allclose(outgrad.shape, (inp.shape[0], param.shape[1], inp.shape[2], inp.shape[3]))
+
+  param_grad = np.zeros(param.shape)
+
+  for obs in range(inp.shape[0]):
+    for outchan in range(param.shape[1]):
+      for inchan in range(param.shape[0]):
+        param_grad[inchan][outchan] += conv_2d_param_grad(inp[obs][inchan], param[inchan][outchan], outgrad[obs][outchan])
+
+  return param_grad
+
+fpg = da_full_paramgrad(full_inp, full_param)
+neutral_full_outgrad = np.ones(full_out.shape)
+bfpg = _param_grad(full_inp, neutral_full_outgrad, full_param)
+assert np.allclose(fpg, bfpg)
+
+evil_full_outgrad = np.random.randn(*full_out.shape)
+fpg2 = da_full_paramgrad(full_inp, full_param, evil_full_outgrad)
+bfpg2 = _param_grad(full_inp, evil_full_outgrad, full_param)
+assert np.allclose(fpg2, bfpg2)
